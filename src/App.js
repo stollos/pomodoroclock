b6a9ec57bdc3dedc1cdf66674ce6d2c2cd3e5954
Robert Thomas Long <robert@robertthomaslong.com>
2019-12-12 18:03:20 -0500

    refactored code

--- src/App.js
+++ src/App.js
@@ -5,87 +5,162 @@ import './App.css';
 
 function App() {
 
-  const [clock, setClock] = useState({
-    session: 25,
-    break: 5,
-    time: 5,
-    status: 'inactive',
-    phase: 'Session'
+  // const [clock, setClock] = useState({
+  //   session: 25,
+  //   break: 5,
+  //   time: 5,
+  //   status: 'inactive',
+  //   phase: 'Session'
+  // });
+
+  const [session, setSession] = useState({
+    state: 25
   });
 
-  const intervalID = useRef();
+  const [breako, setBreako] = useState({
+    state: 5
+  });
+
+  const [time, setTime] = useState({
+    state: 5
+  });
+
+  const [status, setStatus] = useState({
+    state: 'inactive'
+  });
+
+  const [phase, setPhase] = useState({
+    state: 'Session'
+  });
+
+
+  const [intervalID, setIntervalID] = useState({
+    state: useRef()
+  });
 
   const timer = () => {
-    let min = Math.floor(clock.time / 60);
-    let sec = clock.time - min * 60;
+    let min = Math.floor(time.state / 60);
+    let sec = time.state - min * 60;
     sec = sec < 10 ? '0' + sec : sec;
     min = min < 10 ? '0' + min : min;
-    return min + ':' + sec;
+    return min + ' : ' + sec;
   }
 
   const decBreak = () => {
-    if (clock.break > 1 && clock.status === 'inactive') clock.phase === 'Break' ? setClock({...clock, break: clock.break -= 1,  time: clock.break * 60}) : setClock({...clock, break: clock.break -= 1});
+    if (breako.state > 1 && status.state === 'inactive') {
+      if(phase.state === 'Break'){
+        setBreako({state: --breako.state});
+        setTime({state: breako.state * 60})
+      } else {
+         setBreako({state: --breako.state});
+      }
+    }
   }
 
   const incBreak = () => {
-    clock.phase === 'Break' ? setClock({...clock, break: clock.break += 1,  time: clock.break * 60}) : setClock({...clock, break: clock.break += 1});  }
+    if (phase.state === 'Break' && status.state === 'inactive'){
+      setBreako({state: ++breako.state});
+      setTime({state: breako.state * 60});
+     } else {
+       setBreako({state: ++breako.state});
+     }
+  }
 
   const decSession = () => {
-    if (clock.session > 1 && clock.status === 'inactive')
-    clock.phase === 'Session' ? setClock({...clock, session: clock.session -= 1,  time: clock.session * 60}) : setClock({...clock, session: clock.session -= 1});
+    if (session.state > 1 && status.state === 'inactive') {
+      if(phase.state === 'Session'){
+        setSession({state: --session.state});
+        setTime({state: session.state * 60});
+       } else {
+        setSession({state: --session.state});
+       }
+    }
   }
 
   const incSession = () => {
-    clock.phase === 'Session' ? setClock({...clock, session: clock.session += 1,  time: clock.session * 60}) : setClock({...clock, session: clock.session += 1});
+    if(phase.state === 'Session' && status.state === 'inactive') {
+      setSession({state: ++session.state});
+      setTime({state: session.state * 60})
+    } else {
+      setSession({state: ++session.state});
+    }
   }
 
   const start_stop = () => {
-    clock.status === 'inactive' ? intervalID.current = setInterval(startTimer, 1000) : stopTimer();
+    if(status.state === 'inactive'){
+      setStatus({state: 'active'});
+      // startTimer();
+    } else {
+      // stopTimer();
+      setStatus({state: 'inactive'});
+    }
+    console.log(status.state);
   }
 
+  // react batches state updates that occur in event handlers. so how do i fix this?
+  
   const startTimer = () => {
-    setClock({...clock, time: clock.time -= 1, status: 'active'})
-    phaseManager();
+    setIntervalID({current: setInterval(function(){
+        decrementTimer();
+        phaseManager();
+      }, 1000)
+    });
+  }
 
-    // if (clock.time === 0 && clock.phase === 'Session') setClock({...clock, time: clock.break * 60, phase: 'Break'});
-    // else if (clock.time === 0 && clock.phase === 'Break') setClock({...clock, time: clock.session * 60, phase: 'Session'});
-    // else setClock({...clock, time: clock.time -= 1, status: 'active'});
-
-    console.log(clock.status);
+  const decrementTimer = () => {
+    setTime({state: --time.state});
   }
 
   const stopTimer = () => {
-    setClock({...clock, status: 'inactive'});
-    clearInterval(intervalID.current)
+    setIntervalID({current: clearInterval(intervalID.current)});
+    setStatus({state: 'inactive'});
+  }
+
+  const switchBreak = () => {
+    setTime({state: breako.state * 60});
+    setPhase({state: 'Break'});
+    // start_stop();
+    console.log('begin break');
+  }
+
+  const switchSession = () => {
+    setTime({state: session.state * 60});
+    setPhase({state: 'Session'});
+    // start_stop();
+    console.log('begin session');
   }
 
   const phaseManager = () => {
-    if (clock.time === 0 && clock.phase === 'Session') setClock({...clock, time: clock.break * 60, phase: 'Break'});
-    if (clock.time === 0 && clock.phase === 'Break') setClock({...clock, time: clock.session * 60, phase: 'Session'});
+    if(time.state === 0){
+      phase.state === 'Session' ? switchBreak() : switchSession();
+    }
+    console.log(time.state);
+    console.log(status.state); 
   }
+    
 
   // ohhhhhhh great, setState is asynchronous, no setting state multiple times in a single call. this will overwrite previous properties written to that state.
 
   const resetClock = () => {
     stopTimer();
 
-    setClock({
-      session: 25,
-      break: 5,
-      time: 1500,
-      status: 'inactive',
-      phase: 'Session'
-    })
+    // setClock({
+    //   session: 25,
+    //   break: 5,
+    //   time: 1500,
+    //   status: 'inactive',
+    //   phase: 'Session'
+    // })
   }
 
   return (
     <div className='app'>
-      <p id='timer-label'>{clock.phase}</p>
+      <p id='timer-label'>{phase.state}</p>
       <div className='holder'>
         <div id="session">
           <label id="session-label">Session Length</label>
           <button id='session-increment' onClick={ () => incSession() }>ADD</button>
-          <p id='session-length'>{clock.session}</p>
+          <p id='session-length'>{session.state}</p>
           <button id='session-decrement' onClick={ () => decSession() }>SUBTRACT</button>
         </div>
         <div className='clock'>
@@ -95,9 +170,9 @@ function App() {
         </div>
         <div id="break">
           <label id="break-label">Break Length</label>
-          <button id='break-increment' onClick={ () => incBreak() }>ADD</button>
-          <p id='break-length'>{clock.break}</p>
-          <button id='break-decrement' onClick={ () => decBreak() }>SUBTRACT</button>
+          <button id='break-increment' onClick={ () => incBreak()}>ADD</button>
+          <p id='break-length'>{breako.state}</p>
+          <button id='break-decrement' onClick={ () => decBreak()}>SUBTRACT</button>
         </div>
       </div>
       <div className='controls'>
